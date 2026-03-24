df74c4770eb988785d6b4a47b3ade6ad99ffab43
utakaha <18yukitaka@gmail.com>
2026-03-25 00:38:14 +0900

    Expose the Atom feed at /feed

--- lib/build.rb
+++ lib/build.rb
@@ -15,7 +15,7 @@ end
 class Build
   ROOT_PATH = File.expand_path('..', __dir__)
   BASE_URL = 'https://utakaha.com'
-  FEED_URL = "#{BASE_URL}/feed.xml"
+  FEED_URL = "#{BASE_URL}/feed"
   FEED_TITLE = '@utakaha'
   FEED_AUTHOR = 'utakaha'
 
@@ -25,6 +25,7 @@ class Build
     build_index_page
     build_post_pages
     build_atom_feed
+    build_cloudflare_pages_config
     build_404_page
   end
 
@@ -98,6 +99,26 @@ class Build
     feed = renderer.render
     File.open(File.expand_path('public/feed.xml', ROOT_PATH), 'w') do |file|
       file.write(feed)
+    end
+  end
+
+  def build_cloudflare_pages_config
+    headers = <<~HEADERS
+      /feed
+        Content-Type: application/atom+xml; charset=UTF-8
+
+      /feed.xml
+        Content-Type: application/atom+xml; charset=UTF-8
+    HEADERS
+    File.open(File.expand_path('public/_headers', ROOT_PATH), 'w') do |file|
+      file.write(headers)
+    end
+
+    redirects = <<~REDIRECTS
+      /feed /feed.xml 200
+    REDIRECTS
+    File.open(File.expand_path('public/_redirects', ROOT_PATH), 'w') do |file|
+      file.write(redirects)
     end
   end
 
